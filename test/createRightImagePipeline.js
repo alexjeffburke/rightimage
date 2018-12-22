dd9a64917224fc9f723e16cc7e86575489eb862a
Alex J Burke <alex@alexjeffburke.com>
2018-12-22 23:41:31 +0100

    Ensure Gifsicle is used for image/gif tranformations.
    This commit ensures the Gifsicle library is available and thus is
    picked up by the express-processimage pipeline. Add tests.
    
    Cover the of a rotation of zero being supplied for GIFs. While the
    pipeline correctly skips doing work in this case, it unfortunately
    returns something that is not a stream to we special case this and
    instead return a PassThrough stream so callers are unaffected.

--- test/createRightImagePipeline.js
+++ test/createRightImagePipeline.js
@@ -6,6 +6,20 @@ const TEST_DATA_PATH = path.resolve(__dirname, "..", "testdata");
 
 const createRightImagePipeline = require("../lib/createRightImagePipeline");
 
+function createPromiseFromStream(stream) {
+    return new Promise(function(resolve, reject) {
+        var chunks = [];
+        stream
+            .on("data", function(chunk) {
+                chunks.push(chunk);
+            })
+            .on("end", function() {
+                resolve(Buffer.concat(chunks));
+            })
+            .on("error", reject);
+    });
+}
+
 describe("createRightImagePipeline", () => {
     it("should allow image options to be post-processed", () => {
         const imageFileStream = fs.createReadStream(
@@ -34,6 +48,60 @@ describe("createRightImagePipeline", () => {
 
             expect(sawPostProcessCall, "to be true");
             expect(outputTransformed, "to be false");
+        });
+    });
+
+    describe("image/gif", () => {
+        it("should pass through an image", () => {
+            const imageFileStream = fs.createReadStream(
+                path.join(TEST_DATA_PATH, "test.gif")
+            );
+            const imageFileBuffer = fs.readFileSync(
+                path.join(TEST_DATA_PATH, "test.gif")
+            );
+
+            return expect(function(cb) {
+                createRightImagePipeline(
+                    {
+                        contentType: "image/gif",
+                        inputStream: imageFileStream,
+                        imageOptions: { rotate: 0 }
+                    },
+                    cb
+                );
+            }, "to call the callback without error")
+                .then(([{ outputStream }]) =>
+                    createPromiseFromStream(outputStream)
+                )
+                .then(outputBuffer => {
+                    expect(outputBuffer, "to equal", imageFileBuffer);
+                });
+        });
+
+        it("should pass through an image when rotation is specified but not required", () => {
+            const imageFileStream = fs.createReadStream(
+                path.join(TEST_DATA_PATH, "test.gif")
+            );
+            const imageFileBuffer = fs.readFileSync(
+                path.join(TEST_DATA_PATH, "test.gif")
+            );
+
+            return expect(function(cb) {
+                createRightImagePipeline(
+                    {
+                        contentType: "image/gif",
+                        inputStream: imageFileStream,
+                        imageOptions: { rotate: 0 }
+                    },
+                    cb
+                );
+            }, "to call the callback without error")
+                .then(([{ outputStream }]) =>
+                    createPromiseFromStream(outputStream)
+                )
+                .then(outputBuffer => {
+                    expect(outputBuffer, "to equal", imageFileBuffer);
+                });
         });
     });
 });
